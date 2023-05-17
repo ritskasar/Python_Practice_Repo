4d4d4583868384485a0c171ebce21379a2183b92
Rutika Dilip Kasar <rutikakasar@gmail.com>
2023-05-17 06:53:51 +0530

    Programs using python

--- typeConversion.py
+++ typeConversion.py
@@ -1,3 +1,3 @@
 old_age = input("What is your old age : ")
 new_age = int(old_age) + 5
-print("new age : " ,  new_age)
+print("new age : ", new_age)
---
+++ while_loop_pro.py
@@ -0,0 +1,13 @@
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+    
+    
+print("star pattern")
+
+j = 5
+while j >= 0:
+    print(j * '*')
+    j -= 1
