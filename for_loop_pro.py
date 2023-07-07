bfb2b94d270b560cc9b423c58482629599b53ed5
Rutika Dilip Kasar <rutikakasar@gmail.com>
2023-07-07 13:33:35 +0530

    Python practice . .

---
+++ for_loop_pro.py
@@ -0,0 +1,7 @@
+# for loop is apply in python when the number of iteration fixed in program . . 
+
+for i in range(5):
+    print(i)
+    i += 1
+
+print("Numbers of range . .")
---
+++ list_in_python.py
@@ -0,0 +1,9 @@
+marks = [98, 99, 100, "Maths"]
+print(marks)
+print(marks[1])   # in list print by index number . .
+print(marks[-1: -3])   # also indexing in negative . .
+print(marks[0: 2])
+
+# using for loop in list . . 
+for score in marks:
+    print(score)
